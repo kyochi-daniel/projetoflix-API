9807bfc97c55c3cdc3fdf6a5a577f370919347d4
Daniel Kyochi <michigamidaniel77@gmail.com>
2023-03-06 12:27:12 -0400

    initial commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import './header.css'
+import { Link } from 'react-router-dom'
+
+function Header() {
+    return(
+        <header>
+            <Link className='logo' to="/">Prime<span>Flix</span></Link>
+            <Link className='favoritos' to="/favoritos">Meu filmes</Link>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/pages/Filme/index.js
@@ -0,0 +1,84 @@
+import { useEffect, useState } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+import './filme-info.css'
+
+import api from '../../services/api'
+
+function Filme(){
+    const { id } = useParams()
+    const navigate = useNavigate()
+
+    const [filme, setFilme] = useState({})
+    const [loading, setLoading] = useState(true)
+
+    useEffect(()=> {
+        async function loadFIlme(){
+            await api.get(`/movie/${id}`, {
+                params:{
+                    api_key: "dc3a1dc12322567da2d631d3d836a6a0",
+                    language: "pt-BR",
+                }
+            })
+            .then((response)=>{
+                setFilme(response.data)
+                setLoading(false)
+            })
+            .catch(()=>{
+                navigate("/", { replace: true })
+                return
+            })
+        }
+
+        loadFIlme()
+
+        return () => {
+            console.log('COMPONENTE FOI DESMONTADO')
+        }
+    }, [navigate, id])
+
+    function salvarFilme() {
+        const minhaLista = localStorage.getItem('@primeflix')
+
+        let filmesSalvos = JSON.parse(minhaLista) || []
+
+        const hasFilme = filmesSalvos.some((filmesSalvos)=> filmesSalvos.id === filme.id )
+
+        if(hasFilme) {
+            alert('ESSE FILME JÁ ESTÁ NA LISTA')
+            return
+        }
+
+        filmesSalvos.push(filme)
+        localStorage.setItem('@primeflix', JSON.stringify(filmesSalvos))
+        alert('FILME SALVO COM SUCESSO')
+    }
+
+    if(loading) {
+        return(
+            <div className='filme-info'>
+                <h1>Carregando detalhes...</h1>
+            </div>
+        )
+    }
+
+    return(
+        <div className='filme-info'>
+            <h1>{filme.title}</h1>
+            <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title}/>
+
+            <h3>Sinopse</h3>
+            <span>{filme.overview}</span>
+
+            <strong>Avaliação: {filme.vote_average.toFixed(1)} /10</strong>
+
+            <div className='area-buttons'>
+                <button onClick={salvarFilme}>Salvar</button>
+                <button>
+                    <a target="blank" rel='externalcls' href={`https://youtube.com/results?search_query=${filme.title} Trailer`}>Trailer</a>
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Filme

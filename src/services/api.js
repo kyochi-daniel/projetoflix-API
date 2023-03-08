f7c571854b41de6d91c5fffe8beaaab95ac7160f
Daniel Kyochi <michigamidaniel77@gmail.com>
2023-03-07 20:21:11 -0400

    add configs css

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,5 @@
 import axios from 'axios'
 
-//Base da URL: https://api.themoviedb.org/3/
-//URL DA API: /movie/now_playing?api_key=dc3a1dc12322567da2d631d3d836a6a0&language=pt-BR
-
 const api = axios.create({
     baseURL: 'https://api.themoviedb.org/3/'
 })
